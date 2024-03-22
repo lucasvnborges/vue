50b7fe577c9e09fe18f49e8015c7e29231b3c30f
Lucas Borges <lucasvnborges@gmail.com>
2024-03-22 17:47:24 -0300

    chore: vue router

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,30 @@
 <template>
-  <p>Uncompleted Todos:</p>
-
-  <div
-    :key="todo.id"
-    :class="{ red: !todo.done }"
-    v-for="todo in uncompleted"
-  >
-    {{ todo.task }} - <span>{{ todo.done }}</span>
-    <input type="checkbox" v-model="todo.done" />
-  </div>
-
-  <p>Completed Todos:</p>
-  <div
-    :key="todo.id"
-    :class="{ blue: todo.done }"
-    v-for="todo in completed"
-  >
-    {{ todo.task }} - <span>{{ todo.done }}</span>
-    <input type="checkbox" v-model="todo.done" />
-  </div>
-
-  <br><br>
-
-  <select v-model="selectedOption.option">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-  </select>
-
-  <slot name="description" />
-
-  <p>
-    {{ selectedOption.option }}
-  </p>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-
-export default {
-  name: "App",
-  components: {},
-  beforeCreate() {
-    console.log('beforeCreate')
-    console.log('DOM: ', this.$el)
-  },
-  created() {
-    console.log('created')
-    console.log('DOM: ', this.$el)
-  },
-  beforeMount() {
-    console.log('beforeMount')
-    console.log('DOM: ', this.$el)
-  },
-  mounted() {
-    console.log('mounted')
-    console.log('DOM: ', this.$el)
-  },
-  beforeUnmount() {
-    console.log('beforeUnmount')
-  },
-  unmounted() {
-    console.log('unmounted')
-  },
-  data() {
-    return {
-      selectedOption: {
-        option: 0
-      },
-      todos: [
-        { id: 1, task: "item 1", done: false },
-        { id: 2, task: "item 2", done: false },
-        { id: 3, task: "item 3", done: false },
-        { id: 4, task: "item 4", done: true },
-      ],
-      isBlue: true,
-      isRed: false,
-    };
-  },
-  watch: {
-    selectedOption: {
-      handler() {
-        console.log('option alterada')
-      },
-      deep: true
-    }
-  },
-  methods: {
-    selectedOptionWasChanged() {
-      console.log('selected option was changed')
-    }
-  },
-  computed: {
-    uncompleted() {
-      return this.todos.filter(todo => !todo.done)
-    },
-    completed() {
-      return this.todos.filter(todo => todo.done)
-    }
-  }
-};
-</script>
-
 <style>
-.blue {
-  color: blue;
-}
-
-.red {
-  color: red;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
